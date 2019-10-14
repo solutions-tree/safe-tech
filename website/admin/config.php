37bbdd0648539cab3c14f628fa7241704b0af616
solutions-tree <pawanrohera55@gmail.com>
2019-10-14 16:12:39 +0530

    website

---
+++ website/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://safetech.solutions/admin/');
+define('HTTP_CATALOG', 'http://safetech.solutions/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://safetech.solutions/admin/');
+define('HTTPS_CATALOG', 'http://safetech.solutions/');
+
+// DIR
+define('DIR_APPLICATION', '/home/safetech/public_html/admin/');
+define('DIR_SYSTEM', '/home/safetech/public_html/system/');
+define('DIR_IMAGE', '/home/safetech/public_html/image/');
+define('DIR_STORAGE', '/home/safetech/storage/');
+define('DIR_CATALOG', '/home/safetech/public_html/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'safetech_shop');
+define('DB_PASSWORD', 'Pawan@9988');
+define('DB_DATABASE', 'safetech_shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
